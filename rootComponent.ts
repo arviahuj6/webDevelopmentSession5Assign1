0e70ed9b7b2a9e5b0d1c920b05d1f89e7f9445b1
Arvind Ahuja <geetika.arvind@gmail.com>
2017-09-27 19:06:24 +0530

    Add files via upload

---
+++ rootComponent.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+// Decorate the with @Component().
+@Component({
+  selector: 'my-app-component'
+})
+
+// Simple Class with name RootComponent
+export class RootComponent {
+}
